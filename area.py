db27663c6526b10e295fa263aa16599e5dd5c922
Muhammad Rizwan <dua44@hotmail.com>
2024-05-08 14:37:43 +0500

    ------

--- area.py
+++ area.py
@@ -1,5 +1,7 @@
 # https://www.youtube.com/watch?v=0cQj7q8EWxY&list=PLeo1K3hjS3uv5U-Lmlnucd7gqF-3ehIh0&index=8
 base = 10
 height = 5
-area = 1 / 2 * base * height
+print("Base of triangle is", base)
+print("Height of triangle is", height)
+area = 1 / 2 * (base * height)
 print("Area of triangle is", area)
